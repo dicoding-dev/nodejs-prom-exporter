4fe17950f57193733fdc0b45913c911f14ca8a99
Habibi Mustafa <bibitahabibi@gmail.com>
2023-04-03 09:33:22 +0700

    bismillah

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { collectDefaultMetrics, register } from 'prom-client';
+
+collectDefaultMetrics({
+    labels: { NODE_APP_INSTANCE: process.env.NODE_APP_INSTANCE },
+});
+
+const app = express();
+
+app.get('/metrics', async (_req, res) => {
+    try {
+        res.set('Content-Type', register.contentType);
+        res.end(await register.metrics());
+    } catch (err) {
+        res.status(500).end(err);
+    }
+});
+
+app.listen(4001, '0.0.0.0');
